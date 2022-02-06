b018b05ce9fba382d71935ea6acac40b120833a3
devin <devinreina99@gmail.com>
2022-02-06 14:48:43 -0800

    finished application

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,8 @@ var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
 
+var languageButtonsEl = document.querySelector("#language-buttons")
+
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
@@ -89,6 +91,34 @@ var displayRepos = function(repos, searchTerm) {
     repoContainerEl.appendChild(repoEl);
   }
 };
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if(response.ok){
+            response.json().then(function(data) {
+                //console.log(data);
+                displayRepos(data.items, language);
+            });
+            //console.log(response);
+        }
+        else {
+            alert("Error: Github User Not Found");
+        }
+    });
+};
+
+var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    //console.log(language);
+    if(language) {
+        getFeaturedRepos(language);
+        repoContainerEl.textContent = "";
+    }
+}
+
+languageButtonsEl.addEventListener("click", buttonClickHandler);
 
 // add event listeners to forms
 userFormEl.addEventListener('submit', formSubmitHandler);
